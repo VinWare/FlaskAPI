56e8357402301a6357f94b532c7c89a53ef79ed9
Vineet Rao <VinWare@users.noreply.github.com>
2018-10-30 01:28:59 +0530

    Login

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,31 @@
-from app import app
+from flask import url_for, redirect, request, jsonify
+
+from app import app, db
+from flask_login import current_user, login_user, logout_user
+
+from app.models import User
+
 
 @app.route('/')
-@app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/index')
+def act_index():
+    if current_user.is_authenticated:
+        return jsonify({'flag':1})
+
+@app.route('/check-login', methods=['POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.query.filter_by(username=request.args['username']).first()
+    if user is None or not user.check_password(request.args['password']):
+        return redirect(url_for('/'))
+    login_user(user)
+    return redirect(url_for('index'))
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
